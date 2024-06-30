a5068420e3ba5c464425e6b9bef52e9de85d91d3
bekalu73 <bekalusisay2010@gmail.com>
2024-06-30 16:44:55 +0300

    complete database connection

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const mysql2 = require("mysql2");
 const app = express();
 
+const dbConfig = {
+  database: "abe-garaj-demo",
+  user: "abeUser",
+  password: "abeUser",
+  host: "localhost",
+  connectionLimit: 10,
+};
+const connection = mysql2.createConnection(dbConfig);
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected");
+});
+
+// route
 app.get("/", function (req, res) {
   console.log("server");
 });
-
 const PORT = 8800;
 app.listen(PORT, () => {
   console.log(`server is listening on http://localhost:${PORT}`);
